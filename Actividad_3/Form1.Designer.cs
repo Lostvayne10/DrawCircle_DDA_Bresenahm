9d710a7ebfaf9a95e393d28b91434fea79906709
ANTONIO ALEJANDRO INFANTE HERNANDEZ <antonio.infante@alumno.udg.mx>
2019-03-06 23:33:42 -0600

    Termino Proyecto

---
+++ Actividad_3/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Actividad_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tiempodda = new System.Windows.Forms.Label();
+            this.tiempobresenham = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(788, 550);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(853, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tiempo de ejecucion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(827, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DDA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(931, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "BRESENHAM";
+            // 
+            // tiempodda
+            // 
+            this.tiempodda.AutoSize = true;
+            this.tiempodda.Location = new System.Drawing.Point(827, 153);
+            this.tiempodda.Name = "tiempodda";
+            this.tiempodda.Size = new System.Drawing.Size(0, 17);
+            this.tiempodda.TabIndex = 4;
+            // 
+            // tiempobresenham
+            // 
+            this.tiempobresenham.AutoSize = true;
+            this.tiempobresenham.Location = new System.Drawing.Point(946, 153);
+            this.tiempobresenham.Name = "tiempobresenham";
+            this.tiempobresenham.Size = new System.Drawing.Size(0, 17);
+            this.tiempobresenham.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 574);
+            this.Controls.Add(this.tiempobresenham);
+            this.Controls.Add(this.tiempodda);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label tiempodda;
+        private System.Windows.Forms.Label tiempobresenham;
+    }
+}
+
---
+++ Actividad_3/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Actividad_3
+{
+    public partial class Form1 : Form
+    {
+        bool band;
+        Pen pen1 = new Pen(Color.Red, 1);
+        Pen pen2 = new Pen(Color.Black, 1);
+        Pen pen3 = new Pen(Color.Blue, 1);
+        Bitmap bmp = new Bitmap(800, 500);
+        int x1, y1,x2,y2, r;
+
+        public Form1()
+        {
+            InitializeComponent();
+            band = true;
+            x1 = x2 = y1 = y2 = r = 0 ;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+           
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (band)
+            {
+                y1 = e.Y;
+                x1 = e.X;
+                panel1.CreateGraphics().DrawEllipse(pen1,x1,y1,1,1);
+                bmp.SetPixel(x1, y1, Color.Red);
+                band = false;
+            }
+            else
+            {
+                y2 = e.Y;
+                x2 = e.X;
+                panel1.CreateGraphics().DrawEllipse(pen1, x1, y1, 1, 1);
+                DrawElipseDDA(x1, y1, SacarRadio(x1, y1, x2, y2));
+                DrawElipseBresenham(x1, y1, SacarRadio(x1, y1, x2, y2));
+                band = true;
+            }
+        }
+
+        private void DrawElipseDDA(int xc, int yc, double r)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            double xi, yi;
+            xi = yi = 0;
+            for(xi = 0; xi<=yi; xi++)
+            {
+                yi = Math.Sqrt(Math.Abs(Math.Pow(r, 2) - Math.Pow(xi, 2)));
+                drawOctante(Math.Round(xi), Math.Round (yi), xc, yc,pen2);
+            }
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            tiempodda.Text = String.Format("{0}", ts.TotalMilliseconds);
+
+        }
+
+        private void DrawElipseBresenham(int xc, int yc, double r)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            double xi, yi;
+            double d;
+            xi = 0;
+            yi = r ;
+            d = (5 / 4) - r;
+            drawOctante(xi, yi, xc, yc,pen3);
+
+            while(yi>xi)
+            {
+                if (d < 0)
+                {
+                    d += 2 * xi + 5;
+                    xi++;
+                }
+                else
+                {
+                    d += 2 * (xi - yi) + 5;
+                    xi++;
+                    yi--;
+                }
+                drawOctante(xi, yi, xc, yc, pen3);
+            }
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            tiempobresenham.Text = String.Format("{0}", ts.TotalMilliseconds);
+        }
+
+        void drawOctante(double x, double y, int xc, int yc, Pen p1)
+        {
+            panel1.CreateGraphics().DrawEllipse(p1, (int)x + xc, (int)y + yc, 1, 1);
+            panel1.CreateGraphics().DrawEllipse(p1, (int)-x + xc, (int)-y + yc, 1, 1);
+            panel1.CreateGraphics().DrawEllipse(p1, (int)-x + xc, (int)y + yc, 1, 1);
+            panel1.CreateGraphics().DrawEllipse(p1, (int)x + xc, (int)-y + yc, 1, 1);
+            panel1.CreateGraphics().DrawEllipse(p1, (int)y + xc, (int)x + yc, 1, 1);
+            panel1.CreateGraphics().DrawEllipse(p1, (int)-y + xc, (int)-x + yc, 1, 1);
+            panel1.CreateGraphics().DrawEllipse(p1, (int)-y + xc, (int)x + yc, 1, 1);
+            panel1.CreateGraphics().DrawEllipse(p1, (int)y + xc, (int)-x + yc, 1, 1);
+          
+        }
+
+        private double SacarRadio(int x1, int y1, int x2, int y2)
+        {
+            double resultado;
+            resultado = 0;
+            int Dx2 = Math.Abs(x2 - x1);
+            int Dy2 = Math.Abs(y2 - y1);
+            resultado = (Math.Sqrt(Math.Pow(Dx2,2) + Math.Pow(Dy2,2)));
+            return resultado;
+        }
+    }
+}
